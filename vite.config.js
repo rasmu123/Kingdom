60dbe4cb54c390169a3d17ef83a1a451d0511aaa
rasmu123 <mortal123@live.dk>
2025-10-05 22:53:55 +0200

    base changed

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://github.com/rasmu123/Kingdom/',
+  base: '/Kingdom/',
   plugins: [react()],
   build: {
     outDir: 'docs'
